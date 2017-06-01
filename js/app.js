9c46bdc0ecd35130250074c4db252748fb40d43d
Aymeric <aymeric.gioia@hotmail.fr>
2017-06-01 16:39:29 +0200

    ajout videotheque, assignation de videos sur les points d'interets

--- js/app.js
+++ js/app.js
@@ -21,8 +21,8 @@
         // carte interactive
         initmap: function(data) {
             var map = new L.Map('cdf_map', { fullscreenControl: true });
-            var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-            var osmAttrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, Imagery © CloudMade';
+            var osmUrl = 'http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png';
+            var osmAttrib = 'Map data © <a href="http://www.thunderforest.com/">Thunderforest</a> contributors, Imagery © CloudMade';
 
             var osm = new L.TileLayer(osmUrl, { minZoom: 10, maxZoom: 19, attribution: osmAttrib });
 
@@ -42,8 +42,8 @@
             //bouton pour ouvrir la galerie sur la map
             $("#dialog").dialog({
                 autoOpen: false,
-                width: 1200,
                 resizable: false,
+                width: "auto",
                 fluid: true,
                 clickOut: false,
                 responsive: true,
@@ -56,16 +56,16 @@
                     duration: 1000
                 }
             });
+
             var bouton = L.easyButton({
                 position: 'bottomleft',
                 states: [{
                     onClick: function(btn, map) {
                         $("#dialog").dialog("open");
-                        $("#cdf_map").css("opacity", "0.1");
                         app.initgallery(data);
                     },
                     title: 'Galerie Photos',
-                    icon: '<img class="center" src="../images/glyphicons-139-picture.png">'
+                    icon: '<img class="center" src="../videos/glyphicons-9-film.png">'
                 }]
             }).addTo(map);
 
@@ -83,98 +83,134 @@
 
 
             //circuit vert clair
-            for (i = 0; i < data.vert.length; i++) {
+            for (i = 0; i < data.patrimoine.length; i++) {
                 //marqueurs                 
-                var markersVert = new Marker(data.vert[i].marqueur, '#18453B', "rgba(0, 171, 57, 0.5)", '#000');
-                var latVert = data.vert[i].geoloc.lat;
-                var longVert = data.vert[i].geoloc.lng;
-                markersVert = L.marker([latVert, longVert], {
+                var markersPatrimoine = new Marker(data.patrimoine[i].marqueur, '#18453B', "rgba(0, 171, 57, 0.5)", '#000');
+                var latPatrimoine = data.patrimoine[i].geoloc.lat;
+                var longPatrimoine = data.patrimoine[i].geoloc.lng;
+                markersPatrimoine = L.marker([latPatrimoine, longPatrimoine], {
                     icon: L.BeautifyIcon.icon({
-                        iconSize: markersVert.iconSize,
-                        borderColor: markersVert.borderColor,
-                        backgroundColor: markersVert.backgroundColor,
-                        isAlphaNumericIcon: markersVert.isAlphaNumericIcon,
-                        text: markersVert.text,
-                        textColor: markersVert.textColor,
-                        innerIconStyle: markersVert.innerIconStyle
+                        iconSize: markersPatrimoine.iconSize,
+                        borderColor: markersPatrimoine.borderColor,
+                        backgroundColor: markersPatrimoine.backgroundColor,
+                        isAlphaNumericIcon: markersPatrimoine.isAlphaNumericIcon,
+                        text: markersPatrimoine.text,
+                        textColor: markersPatrimoine.textColor,
+                        innerIconStyle: markersPatrimoine.innerIconStyle
                     })
                 }).addTo(map);
                 //popup
-                titreVert = data.vert[i].titre;
-                texteVert = data.vert[i].texte;
-                var contentPopupVert = '';
-                contentPopupVert += "<h2>" + titreVert + "</h2>" + "<br><div class='carousel'>";
-                for (j = 0; j < data.vert[i].images.length; j++) {
-                    var imgVert = data.vert[i].images[j].url;
-                    contentPopupVert += '<a href="' + imgVert + '" data-lightbox="' + imgVert + '"><img class="imgPopup" src="' + imgVert + '" alt="' + titreVert + '" data-image="' + imgVert + '"></a>';
+                titrePatrimoine = data.patrimoine[i].titre;
+                textePatrimoine = data.patrimoine[i].texte;
+                var contentPopupPatrimoine = '';
+                contentPopupPatrimoine += "<h2>" + titrePatrimoine + "</h2>" + "<br><div class='carousel'>";
+                for (j = 0; j < data.patrimoine[i].videos.length; j++) {
+                    var vidPatrimoine = data.patrimoine[i].videos[j].url;
+                    contentPopupPatrimoine += '<iframe width="auto" height="auto" src="https://www.youtube.com/embed/' + vidPatrimoine + '" frameborder="0" allowfullscreen></iframe>';
                 }
-                contentPopupVert += "<p>" + texteVert + "</p>";
-                markersVert.bindPopup(contentPopupVert);
+                contentPopupPatrimoine += "<p>" + textePatrimoine + "</p>";
+                markersPatrimoine.bindPopup(contentPopupPatrimoine);
             };
 
             //batiments officiels
-            for (i = 0; i < data.annexes.length; i++) {
+            for (i = 0; i < data.initiative.length; i++) {
                 //marqueurs
-                var markersAnnexes = new Marker(data.annexes[i].marqueur, '#193025', "rgba(0, 100, 0, 0.5)", '#000');
-                var latAnnexes = data.annexes[i].geoloc.lat;
-                var longAnnexes = data.annexes[i].geoloc.lng;
-                markersAnnexes = L.marker([latAnnexes, longAnnexes], {
+                var markersInitiative = new Marker(data.initiative[i].marqueur, '#193025', "rgba(0, 100, 0, 0.5)", '#000');
+                var latInitiative = data.initiative[i].geoloc.lat;
+                var longInitiative = data.initiative[i].geoloc.lng;
+                markersInitiative = L.marker([latInitiative, longInitiative], {
                     icon: L.BeautifyIcon.icon({
-                        iconSize: markersAnnexes.iconSize,
-                        borderColor: markersAnnexes.borderColor,
-                        backgroundColor: markersAnnexes.backgroundColor,
-                        isAlphaNumericIcon: markersAnnexes.isAlphaNumericIcon,
-                        text: markersAnnexes.text,
-                        textColor: markersAnnexes.textColor,
-                        innerIconStyle: markersAnnexes.innerIconStyle
+                        iconSize: markersInitiative.iconSize,
+                        borderColor: markersInitiative.borderColor,
+                        backgroundColor: markersInitiative.backgroundColor,
+                        isAlphaNumericIcon: markersInitiative.isAlphaNumericIcon,
+                        text: markersInitiative.text,
+                        textColor: markersInitiative.textColor,
+                        innerIconStyle: markersInitiative.innerIconStyle
                     })
                 }).addTo(map);
                 //popup
-                titreAnnexes = data.annexes[i].titre;
-                texteAnnexes = data.annexes[i].texte;
-                var contentPopupAnnexes = '';
-                contentPopupAnnexes += "<h2>" + titreAnnexes + "</h2>" + "<br><div class='carousel'>";
-                for (j = 0; j < data.annexes[i].images.length; j++) {
-                    var imgAnnexes = data.annexes[i].images[j].url;
-                    contentPopupAnnexes += '<a href="' + imgAnnexes + '" data-lightbox="' + imgAnnexes + '"><img class="imgPopup" src="' + imgAnnexes + '" alt="' + titreAnnexes + '" data-image="' + imgAnnexes + '"></a>';
+                titreInitiative = data.initiative[i].titre;
+                texteInitiative = data.initiative[i].texte;
+                var contentPopupInitiative = '';
+                contentPopupInitiative += "<h2>" + titreInitiative + "</h2>" + "<br><div class='carousel'>";
+                for (j = 0; j < data.initiative[i].videos.length; j++) {
+                    var vidInitiative = data.initiative[i].videos[j].url;
+                    contentPopupInitiative += '<iframe width="auto" height="auto" src="https://www.youtube.com/embed/' + vidInitiative + '" frameborder="0" allowfullscreen></iframe>';
                 }
-                contentPopupAnnexes += "</div><p>" + texteAnnexes + "</p>";
-                markersAnnexes.bindPopup(contentPopupAnnexes);
+                contentPopupInitiative += "</div><p>" + texteInitiative + "</p>";
+                markersInitiative.bindPopup(contentPopupInitiative);
             };
-
+            for (i = 0; i < data.culture.length; i++) {
+                //marqueurs
+                var markersCulture = new Marker(data.culture[i].marqueur, '#193025', "rgba(0, 100, 0, 0.5)", '#000');
+                var latCulture = data.culture[i].geoloc.lat;
+                var longCulture = data.culture[i].geoloc.lng;
+                markersCulture = L.marker([latCulture, longCulture], {
+                    icon: L.BeautifyIcon.icon({
+                        iconSize: markersCulture.iconSize,
+                        borderColor: markersCulture.borderColor,
+                        backgroundColor: markersCulture.backgroundColor,
+                        isAlphaNumericIcon: markersCulture.isAlphaNumericIcon,
+                        text: markersCulture.text,
+                        textColor: markersCulture.textColor,
+                        innerIconStyle: markersCulture.innerIconStyle
+                    })
+                }).addTo(map);
+                //popup
+                titreCulture = data.culture[i].titre;
+                texteCulture = data.culture[i].texte;
+                var contentPopupCulture = '';
+                contentPopupCulture += "<h2>" + titreCulture + "</h2>" + "<br><div class='carousel'>";
+                for (j = 0; j < data.culture[i].videos.length; j++) {
+                    var vidCulture = data.culture[i].videos[j].url;
+                    contentPopupCulture += '<iframe width="auto" height="auto" src="https://www.youtube.com/embed/' + vidCulture + '" frameborder="0" allowfullscreen></iframe>';
+                }
+                contentPopupCulture += "</div><p>" + texteCulture + "</p>";
+                markersCulture.bindPopup(contentPopupCulture);
+            };
         },
 
-        // phototheque
+        // videotheque
         initgallery: function(data) {
 
-            var photoVert = '';
-            for (x = 0; x < data.vert.length; x++) {
-                var alt = data.vert[x].titre;
-                for (y = 0; y < data.vert[x].images.length; y++) {
-                    var imgVert = data.vert[x].images[y].url;
-                    var creditVert = data.vert[x].images[y].credit;
-                    photoVert += '<img alt="' + alt + '" src="' + imgVert + '" data-image="' + imgVert + '" data-description="' + creditVert + '">';
+            var patrimoineVideo = '';
+            for (x = 0; x < data.patrimoine.length; x++) {
+                var alt = data.patrimoine[x].titre;
+                for (y = 0; y < data.patrimoine[x].videos.length; y++) {
+                    var vidPatrimoine = data.patrimoine[x].videos[y].url;
+                    var creditPatrimoine = data.patrimoine[x].videos[y].credit;
+                    patrimoineVideo += '<img alt="' + alt + '"data-type="youtube" data-videoid="' + vidPatrimoine + '">';
                 }
             };
 
-            var photoAnnexes = '';
-            for (x = 0; x < data.annexes.length; x++) {
-                var alt = data.annexes[x].titre;
-                for (y = 0; y < data.annexes[x].images.length; y++) {
-                    var imgAnnexes = data.annexes[x].images[y].url;
-                    var creditAnnexes = data.annexes[x].images[y].credit;
-                    photoAnnexes += '<img alt="' + alt + '" src="' + imgAnnexes + '" data-image="' + imgAnnexes + '" data-description="' + creditAnnexes + '">';
+            var initiativeVideo = '';
+            for (x = 0; x < data.initiative.length; x++) {
+                var alt = data.initiative[x].titre;
+                for (y = 0; y < data.initiative[x].videos.length; y++) {
+                    var vidInitiative = data.initiative[x].videos[y].url;
+                    var creditInitiative = data.initiative[x].videos[y].credit;
+                    initiativeVideo += '<img alt="' + alt + '"data-type="youtube" data-videoid="' + vidInitiative + '">';
                 }
             };
 
-            $('#gallery').html(photoVert + photoAnnexes);
+            var cultureVideo = '';
+            for (x = 0; x < data.culture.length; x++) {
+                var alt = data.culture[x].titre;
+                for (y = 0; y < data.culture[x].videos.length; y++) {
+                    var vidCulture = data.culture[x].videos[y].url;
+                    var creditCulture = data.culture[x].videos[y].credit;
+                    cultureVideo += '<img alt="' + alt + '"data-type="youtube" data-videoid="' + vidCulture + '">';
+                }
+            };
+            $('#gallery1').html(patrimoineVideo + initiativeVideo + cultureVideo);
 
             unitegallery();
 
-            select(photoVert, photoAnnexes);
-            $(".ui-dialog-titlebar-close").on("click", function() {
-                $("#cdf_map").css("opacity", "1")
-            })
+            // select(photoPatrimoine, photoAnnexes);
+            // $(".ui-dialog-titlebar-close").on("click", function() {
+            //     $("#cdf_map").css("opacity", "1")
+            // })
         }
     }
     app.init();
@@ -196,34 +232,9 @@ function select(photoVert, photoAnnexes) {
 
 //définition galerie
 function unitegallery() {
-    $("#gallery").unitegallery({
-        //main options:
-        grid_padding: 10,
-        grid_space_between_cols: 10,
-        grid_space_between_rows: 10,
-        //gallery options:
-        gallery_theme: "tilesgrid",
-        gallery_width: "100%",
-        gallery_background_color: "œ#C0C0C0",
-        //navigation option:
-        grid_num_rows: 3,
-        theme_navigation_type: "arrows",
-        theme_space_between_arrows: 5,
-        //tile design options:
-        tile_overlay_opacity: 0.4,
-        tile_overlay_color: "#000000",
-        tile_enable_image_effect: false,
-        //tile text panel options:
-        tile_enable_textpanel: true,
-        tile_textpanel_title_text_align: "center",
-        tile_textpanel_always_on: true,
-        tile_textpanel_title_font_size: null,
-        tile_enable_action: true,
-        //lightbox options:
-        lightbox_slider_control_zoom: false,
-        lightbox_textpanel_enable_description: true,
-        lightbox_type: "compact",
-        lightbox_overlay_opacity: 0.8,
-        lightbox_slider_image_border: false
+    $("#gallery1").unitegallery({
+        grid_num_rows:10,
+        theme_navigation_type: "bullets",
+
     });
 };
